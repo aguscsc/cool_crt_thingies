af87625b9ad1291b8c913573197e700fe82d8cc0
aguscsc <aguscorreo2@gmail.com>
2025-12-31 01:07:44 -0300

    first commit

---
+++ oscilloscope/src/main.cc
@@ -0,0 +1,12 @@
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+#include <GL/gl.h>
+#include <SDL2/SDL_video.h>
+
+SDL_GL_SetAttribute(SDL_GLattr attr, int value)
+SDL_GL_CONTEXT_PROFILE_CORE
+
+SDL_CreateWindow(const char *title, int x, int y, int w, int h, Uint32 flags)
+
+
+SDL_GL_CreateContext()
